d7a2688e8a13d63cbaff6aef32631bc80c45afe0
ggtigi <tigi20088@gmail.com>
2024-02-29 21:07:53 +0300

    add dz

--- src/main.ts
+++ src/main.ts
@@ -133,7 +133,7 @@ console.log('test')
         // console.log("Осталось " + remainingHours + " часов " + remainingMinutesOutput + " минут до следующего дня.");
         
 
-            //1
+            //1 на уроке
         // let age: number = 25
         // if (age > 14 && age < 90) {
         //     console.log ('всё ок')
@@ -142,7 +142,7 @@ console.log('test')
         // }
 
 
-        // 2
+        // 2 на уроке
 
 
         // age = 25
@@ -158,6 +158,121 @@ console.log('test')
         // } else {
         //     console.log ('не получается')
         // }
+
+
+
+
+
+
+        // дз2.1 Запросить у пользователя его возраст и определить, кем он
+// является: ребенком (0–2), подростком (12–18), взрослым
+// (18_60) или пенсионером (60– ...).
+// let age = parseInt(prompt('Введите ваш возраст:', '0') as string)
+
+// if (age >= 0 && age <= 2) {
+//   console.log("Вы ребенок");
+// } else if (age >= 12 && age <= 18) {
+//   console.log("Вы подросток");
+// } else if (age > 18 && age <= 60) {
+//   console.log("Вы взрослый");
+// } else if (age > 60) {
+//   console.log("Вы пенсионер");
+// } else {
+//   console.log("Некорректный возраст");
+// }
+
+
+
+// дз2.2 Запросить у пользователя число от 0 до 9 и вывести ему
+// спецсимвол, который расположен на этой клавише (1–!,
+// 2–@, 3–# и т. д).
+// let number = prompt("Введите число от 0 до 9:");
+// switch (number) {
+//     case '0':
+//         console.log(")");
+//         break;
+//     case '1':
+//         console.log("!");
+//         break;
+//     case '2':
+//         console.log("@");
+//         break;
+
+//     case '3':
+//         console.log('#')
+//         break
+
+//     case '4':
+//         console.log('$')
+//         break
+
+//     case '5':
+//         console.log('%')
+//         break
+//     case '6':
+//         console.log('^')
+//         break
+
+//     case '7':
+//         console.log('&')
+//         break
+
+//     case '8':
+//         console.log('*')
+//         break
+//         case '9':
+//             console.log('(')
+//             break
+
+
+
+
+
+//     default:
+//         console.log("Некорректное число");
+// }
+
+
+
+
+// дз2.4  Запросить у пользователя год и проверить, високосный он
+// или нет. Високосный год либо кратен 400, либо кратен 4 и
+// при этом не кратен 100
+
+// let year = parseInt(prompt('Введите год', '0') as string)
+// if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+//   console.log('Год високосный');
+// } else {
+//   console.log('Год не високосный');
+// }
+
+
+
+// 2.3 Запросить у пользователя трехзначное и число и проверить,
+// есть ли в нем одинаковые цифры.
+
+// const userInput: number = 333
+
+// if (userInput >= 100 && userInput <= 999) {
+//     const strUserInput: string = userInput.toString();
+    
+//     if (strUserInput == strUserInput || strUserInput == strUserInput || strUserInput == strUserInput) {
+//         console.log('В числе ${userInput} есть одинаковые числа')
+//     } else {
+//         console.log('В числе ${userInput} нет одинаковых чисел')
+//     }
+// } else {
+//     console.log("Вы ввели некорректное трехзначное число")
+
+// }
+
+// 2.6 Написать конвертор валют. Пользователь вводит количе-
+// ство USD, выбирает, в какую валюту хочет перевести: EUR,
+// UAN или AZN, и получает в ответ соответствующую сумму.
+
+
+
+
 
 
 
